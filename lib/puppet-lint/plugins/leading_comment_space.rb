b681d17ab2de633c6eaf1b20ff105031340d0807
Anh Pham <anhpt379@gmail.com>
2022-05-05 08:01:04 +0200

    Add support for `### Comment`

--- lib/puppet-lint/plugins/leading_comment_space.rb
+++ lib/puppet-lint/plugins/leading_comment_space.rb
@@ -3,18 +3,17 @@ PuppetLint.new_check(:leading_comment_space) do
   def check
     tokens.select { |r| r.type == :COMMENT }.each do |token|
       next if token.value =~ /^\#*$/
+      next if token.value =~ /^\#* /
 
-      unless token.value =~ /^\s/
-        notify :warning,
-               message: "Add space after #",
-               line: token.line,
-               column: token.column,
-               token: token
-      end
+      notify :warning,
+             message: 'Add space after #',
+             line: token.line,
+             column: token.column,
+             token: token
     end
   end
 
   def fix(problem)
-    problem[:token].value = ' ' + problem[:token].value
+    problem[:token].value.gsub!(/^(\#*)/, '\1 ')
   end
 end
